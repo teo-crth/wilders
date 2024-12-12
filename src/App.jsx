e8cb3394a6d701977725f5af9fe1d618debd91ca
teo-crth <teoconrath@gmail.com>
2024-12-12 15:42:30 +0100

    component 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import HelloWorld from './hello'
-import  UserContext from './UserContext';
-import UserProfile from './UserProfile';
+import { useState } from 'react';
+import './App.css';
+import PokemonCard from './components/PokemonCard';
 
 function App() {
   const [isOnline, setIsOnline] = useState(false)
 
   return (
     <>
+    < PokemonCard />
     </>
   )
 }
